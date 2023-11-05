51a4057dd695391b1cfe4b7d82141325c43de17f
Shari Pavoni <shari.pavoni@nc-vision.com>
2023-11-05 22:09:38 +0200

    Experiments

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  return <div className="App">Test app</div>;
+}
+
+export default App;
