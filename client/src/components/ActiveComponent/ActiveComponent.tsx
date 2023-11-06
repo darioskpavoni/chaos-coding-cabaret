af6c5f4057e269959a1b411d004047ef6f4c9ac4
Shari Pavoni <shari.pavoni@nc-vision.com>
2023-11-06 22:09:18 +0200

    Add a drawer

---
+++ client/src/components/ActiveComponent/ActiveComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: red;
+`;
+
+const ActiveComponent = () => {
+  // read active cmp state and return a cmp
+
+  return <Container>Container</Container>;
+};
+
+export default ActiveComponent;
--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
-import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./App.css";
+import Drawer from "../ui/Drawer/Drawer";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
-  return <div className="App">Test app</div>;
+  return (
+    <Container>
+      <Drawer />
+    </Container>
+  );
 }
 
 export default App;
